62e25e04dec75eafe2ff52bdfb194b1f6374c7a0
youngeunchoi-814 <yec9125@naver.com>
2021-10-18 21:07:24 +0900

    HTML + CSS : banner

---
+++ main.js
@@ -0,0 +1,12 @@
+$(".mainSlider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 1800,
+    dots : true, 
+    dotsClass : "main-dots", 
+    appendArrows: false,
+    draggable : false
+});
