4e68a349f9d8bb599360849bca27b544270e9173
youngeunchoi-814 <yec9125@naver.com>
2021-10-22 15:00:06 +0900

    CSS: fyd + store

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ $(".mainSlider").slick({
     appendArrows: false,
     draggable : false
 });
+
+ $('.storeSlider').slick({ //storeSlider
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots : true, 
+        dotsClass : "store-dots", 
+        appendArrows: false,
+        draggable : false
+    });
+
+const arrowUp=document.querySelector('.btn_up');
+arrowUp.addEventListener('click',()=>{
+    arrowup.scrollIntoView('#navbar');
+    arrowUp.scrollIntoView({behavior: "smooth"});
+});
